8d69272be84bd96cf32f42f2d9988235139f15e8
Thomas Qvidahl <thomas.qvidahl@telenor.com>
2019-08-08 15:55:23 +0200

    Coming along.

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,14 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        ChatScreen.id: (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+
+      }
     );
   }
 }
